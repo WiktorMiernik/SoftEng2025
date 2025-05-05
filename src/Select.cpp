3d59130764aef48f2386f4060e6258cd49fe014a
unknown <miernio467@gmail.com>
2025-05-05 20:50:47 +0200

    Selection_sort_05_05_2025

--- src/Select.cpp
+++ src/Select.cpp
@@ -1,18 +1,2 @@
 
 #include "Select.h"
-using namespace std;
-
-// n powinno byc liczba elementow tablicy
-// np. int n = sizeof(arr) / sizeof(arr[0]);
-
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        swap(arr[i], arr[minIndex]);
-    }
-}
