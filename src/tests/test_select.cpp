5f6444d709b1b389071119064b1d4bd646362da0
Wiktor Miernik <126495@uksw.edu.pl>
2025-06-01 13:32:14 +0200

    Poprawa testu selection sort

--- src/tests/test_select.cpp
+++ src/tests/test_select.cpp
@@ -1,44 +1,21 @@
+
 #include <gtest/gtest.h>
 #include "Sorter.h"
 #include "SortData.h"
 
-TEST(test_select, SortFunction)
+TEST(test_select, AddFunction)
 {
     Sorter* sorter = new Sorter();
-
-    // Wybór kontenera: std::vector lub std::list
-#if 0
-    SortData<float, std::list> data;
-#else
     SortData<float, std::vector> data;
-#endif
 
-    // Przykładowe nieposortowane dane
-    std::vector<float> unsorted_data = { 7.6, 8.3, 2.7, 6.4, 3.0, 1.2 };
-    for (auto num : unsorted_data)
-    {
-        data.add(num);
-    }
-
-    // Sortowanie Selection Sort
+    int dsize = 128 + rand() % 100;
+    for (unsigned i = 0; i < dsize; i++) data.add(float(rand() % 1024) / 4096);
     auto error = sorter->sort<float, std::vector, STT_SELECT>(data);
 
-    // Sprawdzamy, czy operacja zakończyła się sukcesem
+    ASSERT_TRUE(std::is_sorted(data.data().begin(), data.data().end()));
     ASSERT_EQ(error, SE_SUCCESS);
-
-    // Sprawdzamy, czy dane są posortowane
-    bool is_sorted = true;
-    for (size_t i = 1; i < data.size(); ++i)
-    {
-        if (data[i - 1] > data[i])
-        {
-            is_sorted = false;
-            break;
-        }
-    }
-
-    ASSERT_TRUE(is_sorted) << "Selection Sort NIE działa poprawnie!";
 }
+
 
 int main(int argc, char** argv)
 {
