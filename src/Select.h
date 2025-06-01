a1ea102d9b8383c7312394f95cd0d28614365619
Wiktor Miernik <126495@uksw.edu.pl>
2025-06-01 12:13:10 +0200

    naprawa formatu kodu

--- src/Select.h
+++ src/Select.h
@@ -1,33 +1,22 @@
-#ifndef _SELECT_H
-#define _SELECT_H
-
-#include "SortData.h"
-#include "SortError.h"
-#include "SortTech.h"
-
-template <class T, template <typename...> class Container>
-class Select : public SortTech<T, Container> {
-public:
-    inline SortError sort(SortData<T, Container> &data) override;
-};
-
-template <class T, template <typename...> class Container>
-inline SortError Select<T, Container>::sort(SortData<T, Container> &data) {
+inline SortError Select<T, Container>::sort(SortData<T, Container> &data)
+{
     size_t n = data.size();
 
-    for (size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < n; ++i)
+    {
         size_t min_idx = i;
-        for (size_t j = i + 1; j < n; ++j) {
-            if (data[j] < data[min_idx]) {
+        for (size_t j = i + 1; j < n; ++j)
+        {
+            if (data[j] < data[min_idx])
+            {
                 min_idx = j;
             }
         }
-        if (min_idx != i) {
+        if (min_idx != i)
+        {
             data.swap(i, min_idx);
         }
     }
 
     return SE_SUCCESS;
 }
-
-#endif
